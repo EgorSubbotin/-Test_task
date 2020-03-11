d07a001ab679e9cda6cc12cd58b2b1fa7fb8b935
egor_subbotin <subbotineg@mail.ru>
2020-03-11 10:38:39 +0300

    Change script ControlPlayer in testProg_1. Modify Rotate on Y.

--- testProg_1/Assets/Scripts/ControlPlayer.cs
+++ testProg_1/Assets/Scripts/ControlPlayer.cs
@@ -5,20 +5,14 @@ using UnityEngine.UI;
 
 public class ControlPlayer : MonoBehaviour
 {
-    public static float moveSpeed = 40f;
-    
-    Rigidbody myBody;
-    public Transform playerCamera;
-
-    Vector3 right;
+    public static float moveSpeed = 15f;
+    Rigidbody myBody;  
     float mouseX;
-
     public float jumpForce = 1000f;
     
     void Start()
     {
-
-        myBody = GetComponent<Rigidbody>();
+        myBody = GetComponent<Rigidbody>();        
     }
 
     void Update()
@@ -28,33 +22,12 @@ public class ControlPlayer : MonoBehaviour
             Vector3 down = Vector3.Project(myBody.velocity, transform.up);
 
             Vector3 forward = transform.forward * moveSpeed;
-            if (Input.GetAxis("Mouse X") > 0)
-            {
-                 right = transform.right * 1f;
-            }
-            if (Input.GetAxis("Mouse X") < 0)
-            {
-                right = transform.right * (-1f);
-            }
-            //Vector3 right = transform.right * Input.GetAxis("Mouse X") * 10;
-            //myBody.AddForce((forward + right) * Time.fixedDeltaTime * 2000);
-            myBody.velocity = down+forward + right;
-
-            if (transform.position.y < 0)
-            {
-                mouseX = -Input.GetAxis("Mouse X") * 4;
-            }
-
-            if (transform.position.y > 0)
-            {
-                mouseX = Input.GetAxis("Mouse X") * 4;
-            }
-
-
-            transform.Rotate(transform.up, mouseX);
-            // playerCamera.Rotate(-Input.GetAxis("Mouse Y") * 2, 0, 0);
-            //Debug.Log("Axis"+Input.GetAxis("Mouse X"));
-            // Debug.Log("rot" + transform.rotation.y);
+            
+            myBody.velocity = down+ forward;
+                       
+            mouseX = +Input.GetAxis("Mouse X") *2;
+            
+            transform.Rotate(new Vector3(0,1,0), mouseX);                
         }        
     }
     private void OnCollisionEnter(Collision collision)
